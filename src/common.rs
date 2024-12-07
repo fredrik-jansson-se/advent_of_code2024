13f303bb088697fce0979b64abc718b0986e535a
Fredrik Jansson <fj@avassa.io>
2024-12-07 08:35:47 +0100

    Day6

---
+++ src/common.rs
@@ -0,0 +1,157 @@
+#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
+pub struct Coord(pub isize, pub isize);
+
+impl std::ops::Add<Coord> for Coord {
+    type Output = Coord;
+
+    fn add(self, rhs: Coord) -> Self::Output {
+        (self.0 + rhs.0, self.1 + rhs.1).into()
+    }
+}
+
+impl std::ops::Sub<Coord> for Coord {
+    type Output = Coord;
+
+    fn sub(self, rhs: Coord) -> Self::Output {
+        (self.0 - rhs.0, self.1 - rhs.1).into()
+    }
+}
+
+impl std::fmt::Display for Coord {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+
+impl Coord {
+    //pub fn new(row: isize, col: isize) -> Self {
+    //    (row, col).into()
+    //}
+    //pub fn row(&self) -> usize {
+    //    self.0 as _
+    //}
+    //
+    pub fn irow(&self) -> isize {
+        self.0
+    }
+    //
+    //pub fn col(&self) -> usize {
+    //    self.1 as _
+    //}
+    //
+    pub fn icol(&self) -> isize {
+        self.1
+    }
+    //
+    //pub fn neighbors(&self) -> impl Iterator<Item = Self> {
+    //    [
+    //        (self.irow() - 1, self.icol()).into(),
+    //        (self.irow() + 1, self.icol()).into(),
+    //        (self.irow(), self.icol() - 1).into(),
+    //        (self.irow(), self.icol() + 1).into(),
+    //    ]
+    //    .into_iter()
+    //}
+    //
+    //pub fn manhattan(&self, other: &Self) -> usize {
+    //    ((self.irow() - other.irow()).abs() + (self.icol() - other.icol()).abs()) as usize
+    //}
+}
+
+impl From<(usize, usize)> for Coord {
+    fn from(value: (usize, usize)) -> Self {
+        Self(value.0 as _, value.1 as _)
+    }
+}
+
+impl From<(isize, isize)> for Coord {
+    fn from(value: (isize, isize)) -> Self {
+        Self(value.0 as _, value.1 as _)
+    }
+}
+
+impl From<(i32, i32)> for Coord {
+    fn from(value: (i32, i32)) -> Self {
+        Self(value.0 as _, value.1 as _)
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+pub(crate) enum Dir {
+    N,
+    S,
+    E,
+    W,
+}
+
+impl Dir {
+    //pub(crate) fn turn_left(self) -> Self {
+    //    match self {
+    //        Dir::N => Self::W,
+    //        Dir::S => Self::E,
+    //        Dir::E => Self::N,
+    //        Dir::W => Self::S,
+    //    }
+    //}
+
+    pub(crate) fn turn_right(self) -> Self {
+        match self {
+            Dir::N => Self::E,
+            Dir::S => Self::W,
+            Dir::E => Self::S,
+            Dir::W => Self::N,
+        }
+    }
+
+    pub(crate) fn movement(&self) -> Coord {
+        match self {
+            Dir::N => (-1, 0),
+            Dir::S => (1, 0),
+            Dir::E => (0, 1),
+            Dir::W => (0, -1),
+        }
+        .into()
+    }
+}
+
+#[derive(Clone, Debug, Copy, PartialEq, Eq, Hash)]
+pub struct Pos {
+    pub dir: Dir,
+    pub coord: Coord,
+}
+
+impl Pos {
+    pub fn move_forward(&self) -> Self {
+        let dp = self.dir.movement();
+
+        let coord = self.coord + dp;
+
+        Self {
+            dir: self.dir,
+            coord,
+        }
+    }
+
+    //pub fn turn_right(&self) -> Self {
+    //    Self {
+    //        coord: self.coord,
+    //        dir: self.dir.turn_right(),
+    //    }
+    //}
+
+    //pub fn turn_left(&self) -> Self {
+    //    Self {
+    //        coord: self.coord,
+    //        dir: self.dir.turn_left(),
+    //    }
+    //}
+    //
+    //pub fn row(&self) -> usize {
+    //    self.coord.row()
+    //}
+    //
+    //pub fn col(&self) -> usize {
+    //    self.coord.col()
+    //}
+}
--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,8 @@
+use std::{collections::HashSet};
+use rayon::prelude::*;
+
+use crate::common::{Coord, Dir, Pos};
+
 pub fn run() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("day6.txt")?;
 
@@ -7,21 +12,174 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_1(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+fn run_1(input: &str) -> anyhow::Result<usize> {
+    let mut obstacles: HashSet<Coord> = HashSet::new();
+    let mut pos = crate::common::Pos {
+        coord: (0, 0).into(),
+        dir: crate::common::Dir::N,
+    };
+    let mut num_rows = 0;
+    let mut num_cols = 0;
+    for (row, line) in input.lines().enumerate() {
+        num_rows += 1;
+        for (col, ch) in line.chars().enumerate() {
+            num_cols = num_cols.max(col + 1);
+            match ch {
+                '^' => {
+                    pos.coord = (row, col).into();
+                }
+                '#' => {
+                    obstacles.insert((row, col).into());
+                }
+                _ => {}
+            }
+        }
+    }
+    let mut visited: HashSet<Coord> = HashSet::new();
+    visited.insert(pos.coord);
+    loop {
+        let next_pos = pos.coord + pos.dir.movement();
+        if next_pos.icol() < 0 || next_pos.icol() >= (num_cols as isize) {
+            break;
+        }
+        if next_pos.irow() < 0 || next_pos.irow() >= (num_rows as isize) {
+            break;
+        }
+        if obstacles.contains(&next_pos) {
+            pos.dir = pos.dir.turn_right();
+            continue;
+        }
+        visited.insert(next_pos);
+        pos.coord = next_pos;
+    }
+
+    Ok(visited.len())
 }
 
-fn run_2(_input: &str) -> anyhow::Result<usize> {
-  todo!()
+fn simulate_objects(
+    obstacles: &HashSet<Coord>,
+    extra_obstacle: Coord,
+    start_coord: Coord,
+    num_rows: usize,
+    num_cols: usize,
+) -> bool {
+    let mut visited = HashSet::new();
+    let mut pos = Pos {
+        coord: start_coord,
+        dir: Dir::N,
+    };
+    visited.insert(pos);
+
+    loop {
+        if pos.coord.icol() < 0 || pos.coord.icol() > (num_cols as isize) {
+            return false;
+        }
+        if pos.coord.irow() < 0 || pos.coord.irow() > (num_rows as isize) {
+            return false;
+        }
+
+        let next_pos = pos.coord + pos.dir.movement();
+        visited.insert(pos);
+
+        if obstacles.contains(&next_pos) || next_pos == extra_obstacle {
+            pos.dir = pos.dir.turn_right();
+        } else {
+            pos.coord = next_pos;
+        }
+
+        if visited.contains(&pos) {
+            return true;
+        }
+    }
+}
+
+//1562 - too low
+fn run_2(input: &str) -> anyhow::Result<usize> {
+    let mut obstacles: HashSet<Coord> = HashSet::new();
+    let mut pos = crate::common::Pos {
+        coord: (0, 0).into(),
+        dir: crate::common::Dir::N,
+    };
+    let mut num_rows = 0;
+    let mut num_cols = 0;
+    for (row, line) in input.lines().enumerate() {
+        num_rows += 1;
+        for (col, ch) in line.chars().enumerate() {
+            num_cols = num_cols.max(col + 1);
+            match ch {
+                '^' => {
+                    pos.coord = (row, col).into();
+                }
+                '#' => {
+                    obstacles.insert((row, col).into());
+                }
+                _ => {}
+            }
+        }
+    }
+    let mut visited = Vec::new();
+
+    let start_coord = pos.coord;
+
+    loop {
+        visited.push(pos);
+        let next_pos = pos.coord + pos.dir.movement();
+        if next_pos.icol() < 0 || next_pos.icol() > (num_cols as isize) {
+            break;
+        }
+        if next_pos.irow() < 0 || next_pos.irow() > (num_rows as isize) {
+            break;
+        }
+        if obstacles.contains(&next_pos) {
+            pos.dir = pos.dir.turn_right();
+            continue;
+        }
+        pos.coord = next_pos;
+    }
+
+    let mut added_obstacles = HashSet::new();
+    for v in visited.iter() {
+        let v_next = v.move_forward();
+        if !obstacles.contains(&v_next.coord) && v_next.coord != start_coord {
+            added_obstacles.insert(v_next.coord);
+        }
+    }
+
+    let cnt = added_obstacles
+        .par_iter()
+        .filter(|extra_obstacle| {
+            simulate_objects(
+                &obstacles,
+                **extra_obstacle,
+                start_coord,
+                num_rows,
+                num_cols,
+            )
+        })
+        .count();
+    Ok(cnt)
 }
 
 #[cfg(test)]
 mod tests {
+    const INPUT: &str = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
     #[test]
     fn day6_run_1() {
+        assert_eq!(super::run_1(INPUT).unwrap(), 41);
     }
 
     #[test]
     fn day6_run_2() {
+        assert_eq!(super::run_2(INPUT).unwrap(), 6);
     }
 }
